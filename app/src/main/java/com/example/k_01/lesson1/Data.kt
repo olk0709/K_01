f32dc2b5fd192cac510cdb60a10b563f9915fb27
olk0709 <olk0709@gmail.com>
2023-02-17 09:44:33 +0300

    ДЗ1, ветвление if/try/when(аналог switch в Kotlin)

--- app/src/main/java/com/example/k_01/lesson1/Data.kt
+++ app/src/main/java/com/example/k_01/lesson1/Data.kt
@@ -4,12 +4,19 @@ import android.view.View
 
 data class Data(val id:Int, var age:Int, val weight:Int=20)
 
+enum class TestEnum{
+    test1,
+    test2,
+    test3,
+    test4,
+}
+
 object DataBase{
     fun getTest():String{
-        if (0==0){
+        val result = if (0==0){
             return "test"
         }else{
-            return "бред!"
+             return "бред!"
         }
 
         val impl = object : View.OnClickListener{
@@ -18,4 +25,40 @@ object DataBase{
 
         }
     }
+
+    fun getTestIf():String{
+        val result = if (0==0){
+            val f1 = 1 +123
+            "test"
+        }else{
+            "бред!"
+        }
+        try{
+
+        }catch (e:Throwable){
+
+        }finally {
+            //всегда вызывается
+        }
+
+
+        // Всегда возвращает последнюю строку условия
+        return result
+    }
+
+    fun getTestWhen(input:TestEnum):String{
+        val result = when (input){
+
+
+            //1 -> "1"
+            //2 -> "2"
+            //else -> "default"
+            TestEnum.test1 -> "1"
+            TestEnum.test2 -> "2"
+            TestEnum.test3 -> "3"
+            TestEnum.test4 -> "4"
+        }
+        return result
+    }
+
 }
