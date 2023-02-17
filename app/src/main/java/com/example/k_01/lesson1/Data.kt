ffccf882ed83cf4bf2a88df6cf634eface602587
olk0709 <olk0709@gmail.com>
2023-02-17 09:20:11 +0300

    ДЗ1, анонимный класс (object:)

--- app/src/main/java/com/example/k_01/lesson1/Data.kt
+++ app/src/main/java/com/example/k_01/lesson1/Data.kt
@@ -1,5 +1,7 @@
 package com.example.k_01.lesson1
 
+import android.view.View
+
 data class Data(val id:Int, var age:Int, val weight:Int=20)
 
 object DataBase{
@@ -8,6 +10,12 @@ object DataBase{
             return "test"
         }else{
             return "бред!"
+        }
+
+        val impl = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+            }
+
         }
     }
 }
