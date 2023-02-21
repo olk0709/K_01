d5b35a72a119c157fe9994e03cfa238508ad2ae4
olk0709 <olk0709@gmail.com>
2023-02-21 10:25:10 +0300

    ДЗ 3. Начало

--- app/src/main/java/com/example/k_01/view/MainFragment.kt
+++ app/src/main/java/com/example/k_01/view/MainFragment.kt
@@ -74,7 +74,7 @@ class MainFragment : Fragment() {
 
                 binding.loadingLayout.visibility = View.GONE
                 //отображение результата
-               // binding.message.text = "Получилось"
+               // binding.message.text = "Получилось!"
                 binding.cityName.text=data.weatherData.сity.name.toString()
                 binding.temperatureValue.text = data.weatherData.temperature.toString()
                 binding.feelsLikeValue.text = data.weatherData.feelsLike.toString()
